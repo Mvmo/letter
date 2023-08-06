2f079651945224a189faf53c1cff670657941302
Mvmo <mvmogo@gmail.com>
2023-08-06 17:47:09 +0200

    Remove the list state and use the cursor of textarea

--- src/ui/panel/overview_panel.rs
+++ src/ui/panel/overview_panel.rs
@@ -1,13 +1,12 @@
 use std::{io::Stdout, sync::{mpsc::Receiver, Mutex, Arc}};
 use crossterm::event::{KeyCode, KeyEvent};
-use ratatui::{Frame, backend::CrosstermBackend, widgets::{ListItem, List, ListState}, style::{Style, Color, Modifier}};
+use ratatui::{Frame, backend::CrosstermBackend, widgets::{ListItem, List}, style::{Style, Color, Modifier}};
 use crate::{UpdateResult, AppState, AppMode, ui::textarea::TextArea, Task, TaskState};
 use super::Panel;
 
 pub struct OverviewPanel {
     rx: Arc<Mutex<Receiver<KeyEvent>>>,
     text_area: TextArea<AppState, UpdateResult>,
-    list_state: ListState
 }
 
 impl OverviewPanel {
@@ -28,7 +27,6 @@ impl OverviewPanel {
                 });
 
             app_state.task_store.save();
-
             return (true, UpdateResult::UpdateMode(AppMode::NORMAL));
         };
 
@@ -54,13 +52,8 @@ impl Panel for OverviewPanel {
     fn update(&mut self, app_state: &mut AppState) -> UpdateResult {
         let state = app_state;
         let rx = &self.rx;
-        let list_state = &mut self.list_state;
         let task_store = &mut state.task_store;
         let tasks = &mut task_store.tasks;
-
-        if let None = list_state.selected() {
-            list_state.select(Some(0));
-        }
 
         if let AppMode::INPUT(_) = state.mode {
             return self.text_area.update(rx.clone(), state).unwrap_or(UpdateResult::None);
@@ -68,6 +61,7 @@ impl Panel for OverviewPanel {
 
         let rx = self.rx.lock().unwrap();
         if let Ok(key_event) = rx.try_recv() {
+            let (x, y) = self.text_area.get_cursor();
             match key_event.code {
                 KeyCode::Char('i') => {
                     self.text_area.move_cursor_to_line_end();
@@ -81,46 +75,22 @@ impl Panel for OverviewPanel {
                     return UpdateResult::None;
                 },
                 KeyCode::Char('j') => {
-                    let new_index = match list_state.selected() {
-                        Some(index) => {
-                            if index >= tasks.len() - 1 {
-                                0
-                            } else {
-                                index + 1
-                            }
-                        },
-                        None => 0
-                    };
-
-                    self.text_area.set_cursor((0, new_index));
-                    list_state.select(Some(new_index));
+                    self.text_area.move_cursor_down();
                     return UpdateResult::None;
                 },
                 KeyCode::Char('k') => {
-                    let new_index = match list_state.selected() {
-                        Some(index) => {
-                            if index == 0 {
-                                tasks.len() - 1
-                            } else {
-                                index - 1
-                            }
-                        },
-                        None => 0
-                    };
-
-                    self.text_area.set_cursor((0, new_index));
-                    list_state.select(Some(new_index));
+                    self.text_area.move_cursor_up();
                     return UpdateResult::None;
                 },
                 KeyCode::Char(' ') => {
-                    let mut task = &mut tasks[list_state.selected().unwrap()];
+                    let mut task = &mut tasks[y];
                     task.state = task.state.next();
                     return UpdateResult::Save
                 },
                 KeyCode::Enter => {
-                    let task_index = list_state.selected().unwrap();
-                    let task_text = task_store.tasks[task_index].text.clone();
-                    return UpdateResult::UpdateMode(AppMode::EDIT(task_index, task_text))
+                    // let task_index = list_state.selected().unwrap();
+                    // let task_text = task_store.tasks[task_index].text.clone();
+                    //return UpdateResult::UpdateMode(AppMode::EDIT(task_index, task_text))
                 }
                 _ => {}
             }
@@ -130,8 +100,6 @@ impl Panel for OverviewPanel {
 
     fn draw(&mut self, frame: &mut Frame<CrosstermBackend<Stdout>>, app_state: &AppState) {
         let tasks = &app_state.task_store.tasks;
-        let list_state = &mut self.list_state;
-
         let items: Vec<ListItem> = tasks.iter()
             .map(|task| {
                 ListItem::new(format!("{}", task.state))
@@ -144,7 +112,7 @@ impl Panel for OverviewPanel {
         rect.height = rect.height - 2;
         rect.y = 0;
 
-        frame.render_stateful_widget(my_list, rect, list_state);
+        frame.render_widget(my_list, rect);
 
         let mut text_area_rect = frame.size().clone();
         text_area_rect.x = 2;
@@ -155,6 +123,6 @@ impl Panel for OverviewPanel {
 
 impl OverviewPanel {
     pub fn new(rx: Arc<Mutex<Receiver<KeyEvent>>>) -> Self {
-        OverviewPanel { rx, list_state: ListState::default(), text_area: TextArea::new(vec![]) }
+        OverviewPanel { rx, text_area: TextArea::new(vec![]) }
     }
 }
