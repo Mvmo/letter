4c362ad0084e98fb3254a370b08f693405983f8a
Mvmo <mvmogo@gmail.com>
2023-08-29 22:56:23 +0200

    feat: window manager and windows

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod command;
 mod store;
 mod app;
 
-use std::{path::PathBuf, fs::File, io::Stdout};
+use std::{path::PathBuf, fs::File, io::{Stdout, stdout}};
 
 use app::{Letter, LetterEditor, EditorMode, LetterCommand};
-use ratatui::{prelude::{CrosstermBackend, Rect, Layout, Direction, Constraint}, Terminal};
+use crossterm::terminal::enable_raw_mode;
+use ratatui::{prelude::{CrosstermBackend, Rect, Layout, Direction, Constraint}, Terminal, widgets::{Block, Borders}};
 use rusqlite::Connection;
 use store::TaskStore;
 
@@ -38,7 +39,50 @@ trait Window {
     fn draw(&self, state: &LetterState, frame: &mut Frame, rect: Rect);
 }
 
+struct TestWindow {}
+
+impl Default for TestWindow {
+    fn default() -> Self {
+        return TestWindow {  }
+    }
+}
+
+impl Window for TestWindow {
+    fn update(&mut self, _state: &mut LetterState) -> WindowCommand {
+        None
+    }
+
+    fn draw(&self, _state: &LetterState, frame: &mut Frame, rect: Rect) {
+        let block = Block::default()
+            .title("LKJKLJLK")
+            .borders(Borders::ALL);
+
+        frame.render_widget(block, rect)
+    }
+}
+
+
 struct TaskListWindow {}
+
+impl Default for TaskListWindow {
+    fn default() -> Self {
+        return TaskListWindow {  }
+    }
+}
+
+impl Window for TaskListWindow {
+    fn update(&mut self, _state: &mut LetterState) -> WindowCommand {
+        None
+    }
+
+    fn draw(&self, _state: &LetterState, frame: &mut Frame, rect: Rect) {
+        let block = Block::default()
+            .title("Hallo")
+            .borders(Borders::ALL);
+
+        frame.render_widget(block, rect)
+    }
+}
 
 enum _WindowCommand {
     Quit
@@ -46,23 +90,28 @@ enum _WindowCommand {
 
 type WindowCommand = Option<_WindowCommand>;
 
-impl Window for TaskListWindow {
-    fn update(&mut self, _state: &mut LetterState) -> WindowCommand {
-        None
-    }
-
-    fn draw(&self, _state: &LetterState, _frame: &mut Frame, _rect: Rect) {
-    }
-}
-
 struct WindowManager {
     windows: Vec<Box<dyn Window>>,
     terminal: Terminal<CrosstermBackend<Stdout>>,
 }
 
+impl Default for WindowManager {
+    fn default() -> Self {
+        let windows = vec![];
+
+        let backend = CrosstermBackend::new(stdout());
+        let terminal = Terminal::new(backend).expect("couldn't initiate terminal");
+
+        WindowManager { windows, terminal }
+    }
+}
+
 impl WindowManager {
     fn run(&mut self, store: TaskStore) -> Result<()> {
         let mut state = LetterState::new(store);
+
+        enable_raw_mode()?;
+        self.terminal.clear()?;
 
         loop {
             self.windows.iter_mut()
@@ -71,17 +120,28 @@ impl WindowManager {
                 });
 
             self.terminal.draw(|frame| {
-                // take up equal space for every window horizontally
+                let percentage = 100 / self.windows.len() as u16;
+
+                let constraints: Vec<Constraint> = self.windows.iter()
+                    .map(|_| Constraint::Percentage(percentage))
+                    .collect();
+
                 let layout = Layout::default()
                     .direction(Direction::Horizontal)
-                    .constraints([]);
+                    .constraints(constraints)
+                    .split(frame.size());
 
                 self.windows.iter()
-                    .for_each(|window| {
-                        window.draw(&state, frame, Rect::new(32, 32, 80, 80));
+                    .enumerate()
+                    .for_each(|(idx, window)| {
+                        window.draw(&state, frame, layout[idx]);
                     });
             })?;
         }
+    }
+
+    fn push_window(&mut self, window: Box<dyn Window>) {
+        self.windows.push(window);
     }
 }
 
@@ -102,26 +162,40 @@ fn main() -> Result<()> {
     let mut task_store = TaskStore::new(connection);
     task_store.fetch_data()?;
 
-    let mut letter = Letter::new(task_store);
-    let mut editor = LetterEditor::default();
-
-    editor.init(&letter)?;
-
-    loop {
-        let cmd_out = editor.update(&mut letter);
-        if let Some(cmd) = cmd_out {
-            match cmd {
-                LetterCommand::Quit => break,
-                LetterCommand::Debug => editor.debug_panel_shown = !editor.debug_panel_shown,
-                _ => {}
-            }
-        }
-
-        editor.draw(&mut letter);
-    }
-
-    editor.deinit()?;
+    let mut window_manager = WindowManager::default();
+    window_manager.push_window(Box::new(TaskListWindow::default()));
+    window_manager.push_window(Box::new(TestWindow::default()));
+    window_manager.push_window(Box::new(TestWindow::default()));
+    window_manager.run(task_store)?;
 
     Ok(())
 }
+
+//fn main() -> Result<()> {
+//    let connection = create_database_connection()?;
+//    let mut task_store = TaskStore::new(connection);
+//    task_store.fetch_data()?;
+//
+//    let mut letter = Letter::new(task_store);
+//    let mut editor = LetterEditor::default();
+//
+//    editor.init(&letter)?;
+//
+//    loop {
+//        let cmd_out = editor.update(&mut letter);
+//        if let Some(cmd) = cmd_out {
+//            match cmd {
+//                LetterCommand::Quit => break,
+//                LetterCommand::Debug => editor.debug_panel_shown = !editor.debug_panel_shown,
+//                _ => {}
+//            }
+//        }
+//
+//        editor.draw(&mut letter);
+//    }
+//
+//    editor.deinit()?;
+//
+//    Ok(())
+//}
 
