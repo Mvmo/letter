85deebd6a69a7c5e9f21ed61458ef575973f5d25
Mvmo <mvmogo@gmail.com>
2023-08-30 15:10:17 +0200

    feat: status bar

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod command;
 mod store;
 mod app;
 
-use std::{path::PathBuf, fs::File, io::{Stdout, stdout}};
+use std::{path::PathBuf, fs::File, io::{Stdout, stdout}, fmt::Display};
 
-use app::{Letter, LetterEditor, EditorMode, LetterCommand};
+use app::{Letter, EditorMode, LetterCommand};
 use crossterm::terminal::enable_raw_mode;
-use ratatui::{prelude::{CrosstermBackend, Rect, Layout, Direction, Constraint}, Terminal, widgets::{Block, Borders}};
+use ratatui::{prelude::{CrosstermBackend, Rect, Layout, Direction, Constraint}, Terminal, widgets::{Block, Borders, Paragraph}};
 use rusqlite::Connection;
 use store::TaskStore;
 
@@ -18,6 +18,15 @@ type Frame<'a> = ratatui::Frame<'a, CrosstermBackend<Stdout>>;
 enum LetterMode {
     Normal,
     Insert
+}
+
+impl Display for LetterMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            LetterMode::Normal => f.write_str("NORMAL"),
+            LetterMode::Insert => f.write_str("INSERT")
+        }
+    }
 }
 
 struct LetterState {
@@ -93,51 +102,67 @@ type WindowCommand = Option<_WindowCommand>;
 struct WindowManager {
     windows: Vec<Box<dyn Window>>,
     terminal: Terminal<CrosstermBackend<Stdout>>,
+    state: LetterState
 }
 
-impl Default for WindowManager {
-    fn default() -> Self {
+impl WindowManager {
+    fn new(store: TaskStore) -> Self {
         let windows = vec![];
 
         let backend = CrosstermBackend::new(stdout());
         let terminal = Terminal::new(backend).expect("couldn't initiate terminal");
 
-        WindowManager { windows, terminal }
+        let state = LetterState::new(store);
+
+        WindowManager { windows, terminal, state }
     }
-}
 
-impl WindowManager {
-    fn run(&mut self, store: TaskStore) -> Result<()> {
-        let mut state = LetterState::new(store);
-
+    fn run(&mut self) -> Result<()> {
         enable_raw_mode()?;
         self.terminal.clear()?;
 
         loop {
             self.windows.iter_mut()
                 .for_each(|window| {
-                    window.update(&mut state);
+                    window.update(&mut self.state);
                 });
 
             self.terminal.draw(|frame| {
-                let percentage = 100 / self.windows.len() as u16;
+                let percentage_per_window = 100 / self.windows.len() as u16;
 
                 let constraints: Vec<Constraint> = self.windows.iter()
-                    .map(|_| Constraint::Percentage(percentage))
+                    .map(|_| Constraint::Percentage(percentage_per_window))
                     .collect();
 
-                let layout = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .constraints(constraints)
+                let panel_grid = Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints([
+                        Constraint::Percentage(99),
+                        Constraint::Percentage(1)
+                    ])
                     .split(frame.size());
 
+                let window_grid = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints(constraints)
+                    .split(panel_grid[0]);
+
+                const STATUS_BAR_HEIGHT: u16 = 2;
                 self.windows.iter()
                     .enumerate()
                     .for_each(|(idx, window)| {
-                        window.draw(&state, frame, layout[idx]);
+                        window.draw(&self.state, frame, window_grid[idx]);
                     });
+
+                Self::draw_status_bar(&self.state, frame, panel_grid[1]);
             })?;
         }
+
+    }
+
+    fn draw_status_bar(state: &LetterState, frame: &mut Frame, rect: Rect) {
+        let status_paragraph = Paragraph::new(format!("-- {} --", state.mode));
+        frame.render_widget(status_paragraph, rect);
     }
 
     fn push_window(&mut self, window: Box<dyn Window>) {
@@ -159,14 +184,17 @@ fn create_database_connection() -> Result<Connection> {
 
 fn main() -> Result<()> {
     let connection = create_database_connection()?;
+
     let mut task_store = TaskStore::new(connection);
     task_store.fetch_data()?;
 
-    let mut window_manager = WindowManager::default();
+    //task_store.fetch_data()?;
+
+    let mut window_manager = WindowManager::new(task_store);
     window_manager.push_window(Box::new(TaskListWindow::default()));
     window_manager.push_window(Box::new(TestWindow::default()));
     window_manager.push_window(Box::new(TestWindow::default()));
-    window_manager.run(task_store)?;
+    window_manager.run()?;
 
     Ok(())
 }
