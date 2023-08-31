424e9558560f430046f84d01f30d356e4334dc21
Mvmo <mvmogo@gmail.com>
2023-09-01 00:11:46 +0200

    wip: parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,13 +1,13 @@
 type LSInt = i64;
 type LSFloat = f64;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum NumberLiteral {
     FloatingPoint(LSFloat),
     Integer(LSInt),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Operator {
     Plus,
     Minus,
@@ -19,13 +19,13 @@ enum Operator {
     CmpLessThan
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Keyword {
     Let,
     Fn,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Token {
     Identifier(String),
     Number(NumberLiteral),
@@ -34,6 +34,7 @@ enum Token {
     Semicolon,
     LeftParen,
     RightParen,
+    Eof,
 }
 
 fn as_num(word: String) -> Option<NumberLiteral> {
@@ -119,6 +120,7 @@ fn tokenize(input: String) -> Vec<Token> {
         }
     }
 
+    tokens.push(Token::Eof);
     tokens
 }
 
@@ -130,7 +132,8 @@ fn test_tokenize() {
             vec![
                 Token::Identifier("x".to_string()),
                 Token::Operator(Operator::Assign),
-                Token::Number(NumberLiteral::Integer(5))
+                Token::Number(NumberLiteral::Integer(5)),
+                Token::Eof
             ]
         ),
         (
@@ -140,7 +143,8 @@ fn test_tokenize() {
                 Token::Identifier("x".to_string()),
                 Token::Operator(Operator::Assign),
                 Token::Number(NumberLiteral::Integer(5)),
-                Token::Semicolon
+                Token::Semicolon,
+                Token::Eof
             ]
         ),
         (
@@ -165,6 +169,7 @@ fn test_tokenize() {
                 Token::Operator(Operator::Assign),
                 Token::Number(NumberLiteral::Integer(500)),
                 Token::Semicolon,
+                Token::Eof
             ]
         )
     ];
@@ -185,3 +190,202 @@ fn test_tokenize() {
     }
 }
 
+enum Precedence {
+    Lowest = 0,
+    Sum,
+    Prefix,
+    Call
+}
+
+#[derive(Debug, PartialEq)]
+struct IdentifierExpr {
+    token: Token,
+    value: String
+}
+
+#[derive(Debug, PartialEq)]
+enum Node {
+    ExpressionNode(),
+    StatementNode()
+}
+
+#[derive(Debug, PartialEq)]
+enum Stmt {
+    LetStmt(LetStmt),
+    N
+}
+
+#[derive(Debug, PartialEq)]
+enum Expr {
+    Identifier(IdentifierExpr)
+}
+
+#[derive(Default, Debug, PartialEq)]
+struct ProgramNode {
+    stmt_nodes: Vec<Stmt>
+}
+
+#[derive(Debug)]
+struct Parser {
+    tokens: Vec<Token>,
+    current_tkn_idx: usize,
+    peek_token_idx: usize,
+    program_node: ProgramNode
+}
+
+#[derive(Debug, PartialEq)]
+struct LetStmt {
+    token: Token,
+    identifier: Option<IdentifierExpr>,
+    value: Option<Expr>
+}
+
+impl LetStmt {
+    fn new(token: Token) -> Self {
+        LetStmt { token, identifier: None, value: None }
+    }
+}
+
+struct ExprStmt {
+    token: Token,
+    expression_node: Option<Expr>
+}
+
+//impl Parser {
+//    fn new(tokens: Vec<Token>) -> Self {
+//        Self {
+//            tokens,
+//            current_tkn_idx: 0,
+//            peek_token_idx: 1,
+//            program_node: ProgramNode::default()
+//        }
+//    }
+//
+//    fn parse_program(mut self) -> ProgramNode {
+//        while self.current_token() != &Token::Eof {
+//            let stmt = self.parse_statement();
+//            self.program_node.stmt_nodes.push(stmt);
+//
+//            self.next_token();
+//        }
+//
+//        return self.program_node;
+//    }
+//
+//    fn parse_statement(&mut self) -> Stmt {
+//        match self.current_token() {
+//            Token::Keyword(Keyword::Let) => Stmt::LetStmt(self.parse_let_statement()),
+//            _ => Stmt::N
+//        }
+//    }
+//
+//    fn parse_let_statement(&mut self) -> LetStmt {
+//        let mut stmt_node = LetStmt::new(self.current_token().clone());
+//        let peek_tkn = self.peek_token().clone();
+//        if let Token::Identifier(ident) = peek_tkn {
+//            self.next_token();
+//            let current_token = self.current_token();
+//            stmt_node.identifier = Some(IdentifierExpr { token: current_token.clone(), value: ident.to_string() });
+//        } else {
+//            Self::unexpected_token("Identifier", peek_tkn);
+//        }
+//
+//        let peek_tkn = self.peek_token().clone();
+//        if let Token::Operator(Operator::Assign) = self.peek_token() {
+//            self.next_token();
+//        } else {
+//            Self::unexpected_token("Assign", peek_tkn);
+//        }
+//
+//        self.next_token();
+//
+//        stmt_node.value = Some(self.parse_expression(Precedence::Lowest));
+//        self.skip_semicolon();
+//
+//        stmt_node
+//    }
+//
+//    fn parse_expression_statement(&mut self) -> ExprStmt {
+//        let mut stmt_node = ExprStmt { token: self.current_token().clone(), expression_node: None };
+//        stmt_node.expression_node = Some(self.parse_expression(Precedence::Lowest));
+//
+//        let peek_tkn = self.peek_token();
+//        if let Token::Semicolon = peek_tkn {
+//            self.next_token()
+//        }
+//
+//        stmt_node
+//    }
+//
+//    fn parse_expression(&mut self, precedence: Precedence) -> Expr {
+//        if let Some(left_expr) = self.prefix_parse() {
+//
+//        } else {
+//            panic!("no prefix parse fn found for current token");
+//        }
+//        //return Expr::N
+//    }
+//
+//    fn skip_semicolon(&mut self) {
+//        if let Token::Semicolon = self.peek_token() {
+//            self.next_token()
+//        }
+//    }
+//
+//    fn prefix_parse(&mut self) -> Option<Expr> {
+//        let token = self.current_token();
+//        return match token {
+//            Token::Identifier(_) => Some(Expr::Identifier(self.parse_identifier())),
+//            _ => None,
+//        };
+//    }
+//
+//    fn parse_identifier(&mut self) -> IdentifierExpr {
+//        if let Token::Identifier(ident) = self.current_token() {
+//            IdentifierExpr {
+//                token: self.current_token().clone(),
+//                value: ident.clone(),
+//            }
+//        } else {
+//            Self::unexpected_token("identifier", self.current_token().clone());
+//
+//        }
+//    }
+//
+//    fn next_token(&mut self) {
+//        self.current_tkn_idx = self.peek_token_idx;
+//        self.peek_token_idx += 1;
+//    }
+//
+//    fn peek_token(&self) -> &Token {
+//        self.tokens.get(self.peek_token_idx).unwrap_or(&Token::Eof)
+//    }
+//
+//    fn current_token(&self) -> &Token {
+//        self.tokens.get(self.current_tkn_idx).unwrap_or(&Token::Eof)
+//    }
+//
+//    fn unexpected_token(expected: &str, token: Token) -> ! {
+//        dbg!(token);
+//        panic!("Unexpected token | expected -> {expected}");
+//    }
+//}
+//
+//#[test]
+//fn parser_test() {
+//    let tokens = tokenize("let x = 9;".to_string());
+//    let tokens = dbg!(tokens);
+//    let program = Parser::new(tokens);
+//    let program_node = program.parse_program();
+//    let program_node_expected = ProgramNode {
+//        stmt_nodes: vec![
+//            Stmt::LetStmt(LetStmt {
+//                token: Token::Keyword(Keyword::Let),
+//                identifier: Some(IdentifierExpr { token: Token::Identifier("x".to_string()), value: "x".to_string() }),
+//            })
+//        ]
+//    };
+//
+//    //dbg!(program_node);
+//    assert_eq!(program_node, program_node_expected);
+//}
